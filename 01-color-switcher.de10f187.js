4933fd7cb22efde0a80db847999a4d7ffac7f60e
danibatir <151066482+danibatir@users.noreply.github.com>
2024-05-27 17:43:17 +0000

    Deploying to gh-pages from @ danibatir/goit-js-hw-09@d9a4b4683c0404a97a941ad8bc5e18ac0b3a2591 🚀

---
+++ 01-color-switcher.de10f187.js
@@ -0,0 +1,2 @@
+let t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(()=>{t.disabled=!0,timerId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.de10f187.js.map
