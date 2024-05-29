7c16322327d29ff8f0a7a4e3e75414485464dcdd
danibatir <danibatir@gmail.com>
2024-05-29 13:37:44 +0300

    finished

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+document.querySelector('.form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(this);
+  const firstDelay = parseInt(formData.get('delay'));
+  const step = parseInt(formData.get('step'));
+  const amount = parseInt(formData.get('amount'));
+
+  for (let i = 0; i < amount; i++) {
+    const delay = firstDelay + step * i;
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = { position, delay };
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result);
+      }
+    }, delay);
+  });
 }
