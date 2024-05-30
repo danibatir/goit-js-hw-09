8b115875a365091e69cb0ef078bd25f135ccb512
danibatir <danibatir@gmail.com>
2024-05-30 21:33:13 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
 let inputText = document.getElementById('datetime-picker');
 let buttonStart = document.querySelector('[data-start]');
 const options = {
@@ -19,11 +20,11 @@ const options = {
   },
 };
 
-flatpickr(inputText, options);
+let flatpickrInstance = flatpickr(inputText, options);
 buttonStart.addEventListener('click', () => {
   const endDate = new Date(flatpickrInstance.selectedDates[0].getTime());
   const now = new Date();
-  const diff = endDate - now;
+  let diff = endDate - now;
   let countdownInterval = setInterval(() => {
     const remainingTime = convertMs(diff);
     updateTimerDisplay(
@@ -41,9 +42,22 @@ buttonStart.addEventListener('click', () => {
 });
 
 function updateTimerDisplay(days, hours, minutes, seconds) {
-  const timerElements = document.querySelectorAll('.timer.value');
+  const timerElements = document.querySelectorAll('.timer .value');
   timerElements[0].textContent = String(days).padStart(2, '0');
   timerElements[1].textContent = String(hours).padStart(2, '0');
   timerElements[2].textContent = String(minutes).padStart(2, '0');
   timerElements[3].textContent = String(seconds).padStart(2, '0');
+}
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor((ms % hour) / minute);
+  const seconds = Math.floor((ms % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
